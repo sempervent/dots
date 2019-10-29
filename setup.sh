4734b3a546a7408cb61d804e2b0ac01715ab8b4b
Joshua N. Grant <grantjn@ornl.gov>
2019-10-29 13:15:48 -0400

    updates to vimrc and setup

--- setup.sh
+++ setup.sh
@@ -2,25 +2,30 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null 2>&1 && pwd )"
 SYM_DIR=$DIR/syms
 OLD_DOTS=~/old_dots
+VUNDLE_DIR=~/.vim/bundle/Vundle.vim
+
+if [ ! -d "$OLD_DOTS" ]; then
+  mkdir -p $OLD_DOTS
+fi
 
 move_sym() {
-	if [ "$#" -ne 2 ]; then
-		echo "move_sym expects two arguments:"
-		echo "\tsource"
-		echo "\tdestination"
-		exit 1
-	fi
-	if [ -f "$2" ]; then
-		if [ ! -d "$OLD_DOTS" ]; then
-			mkdir -p $OLD_DOTS
-		fi
-		mv $2 $OLD_DOTS/
-		ln -s $1 $2
-	fi
+  if [ "$#" -ne 2 ]; then
+    echo "move_sym expects two arguments:"
+    echo "\tsource"
+    echo "\tdestination"
+    exit 1
+  fi
+  if [ -f "$2" ]; then
+    mv $2 $OLD_DOTS/
+    ln -s $1 $2
+  fi
 }
 
 move_sym $SYM_DIR/bashrc ~/.bashrc
 move_sym $SYM_DIR/vimrc ~/.vimrc
 move_sym $SYM_DIR/tmux.conf ~/.tmux.conf
 
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+# install vundle into its directory unless it is already installed
+if [ ! -d "$VUNDLE_DIR" ]; then
+  git clone https://github.com/gmarik/Vundle.vim.git $VUNDLE_DIR
+fi
