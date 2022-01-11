86e4829e0d8307cb56a18861823bbb5f5e2f70a4
Joshua Grant <grantjn@ornl.gov>
2022-01-11 07:39:36 -0500

    give more history

--- bash/history.sh
+++ bash/history.sh
@@ -3,7 +3,7 @@ HISTCONTROL=ignoreboth
 # append to the history file
 shopt -s histappend
 # give lots of history
-HISTSIZE=100000   
+HISTSIZE=10000000
 HISTFILESIZE=2000000
 # save and reload history after each command finishes
 export PROMPT_COMMAND="history -a; history -c; history -r; $PROMPT_COMMAND"
