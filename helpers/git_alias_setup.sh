b9020d2b492fa78409f3f99e5d380ab7561f6d0a
sempervent <jngrant9@gmail.com>
2023-02-17 22:04:54 -0500

    move some git helper functions into the alias setup script

--- helpers/git_alias_setup.sh
+++ helpers/git_alias_setup.sh
@@ -2,13 +2,38 @@
 # -*- coding: utf-8 -*-
 git config --global push.autoSetupRemote true
 git config --global pull.rebase true
-git config --global alias.prurl '!f(){ echo \"https://github.com/$(git remote -v | grep origin | awk \''{print $2}\'' | awk -F: \''{print $2}\'' | awk -F. \''{print $1}\'' | head -n 1)/pull/new/$(git symbolic-ref --short HEAD)\"; }; f;'
 git config --global alias.release '!git checkout main && git pull && git checkout -b Deploy_$(date +%F)'
 # shellcheck disable=SC2016
 if [ -x "$(which fortune)" ]; then
   git config --global alias.fortune '!git commit -m \"$(fortune)\"'
 fi
-git config --global alias.print_branch '!git symbolic-ref --short HEAD'
-git config --global alias.print_repo "!git remote -v | grep origin | awk '{print $2}' | awk -F: '{print $2}' | awk -F. '{print $1}' | head -n 1"
-git config --global alias.prurl '! echo "https://github.com/$(git print_remote)/pull/new/$(git print_branch)"'
-
+gitpr() {
+  PREFIX="release/"
+  DATEFMT="%F"
+  while :; do
+    case $1 in
+      -p|--prefix)
+        PREFIX="$2"
+        shift 2
+        ;;
+      -d|--datefmt)
+        DATEFMT="$2"
+        shift 2
+        ;;
+      release)
+        main="$(git branch --list | grep -oP '(master|main)')"
+        git checkout "$main" || exit
+        git pull
+        git checkout -b "${PREFIX}$(date +"${DATEFMT}")"
+        shift
+        ;;
+      url)
+        branch="$(git symbolic-ref --short HEAD)"
+        url="$(git remote -v | grep 'origin.*push' | awk '{print $2}' | grep -oP '((?<=git\@)|((?=https\://)).*(?=\.git)')"
+        echo -e "To make a PR visit:\thttps:$url/pull/new/$branch"
+        shift
+        ;;
+      *) break ;;
+    esac
+  done
+}
