138dcf8e6b5ddb77d50d404940883dd6c8e7025e
sempervent <jngrant9@gmail.com>
2023-02-20 15:59:31 -0500

    get macos version properly

--- bash/functions.sh
+++ bash/functions.sh
@@ -255,7 +255,9 @@ websearch() { # google a query {{{2
 # 1}}
 # system functions {{{1
 whatdistro() {
-  if [ -f /etc/os-release ]; then
+  if [ "$OS" = "Mac" ]; then
+    distro=$(awk '/SOFTWARE LICENSE AGREEMENT FOR macOS/' '/System/Library/CoreServices/Setup Assistant.app/Contents/Resources/en.lproj/OSXSoftwareLicense.rtf' | awk -F 'macOS ' '{print $NF}' | awk '{print substr($0, 0, length($0)-1)}')
+  elif [ -f /etc/os-release ]; then
     distro=$(cat /etc/os-release | grep -oP "(?<=^PRETTY_NAME\=\").*(?=\")")
   else
     distro=$(lsb_release -a | grep "Description" | awk -F: '{print $2}' | \
